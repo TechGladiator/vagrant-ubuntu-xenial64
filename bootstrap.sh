d945b6c61741fd053ed7c1ffa693c243b9025007
John Stratton <jstrat0407@gmail.com>
2017-10-14 21:11:02 -0400

    added bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt install -y git
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y mongodb-org
+sudo service mongod start
