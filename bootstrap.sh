0cf06e4af7e96616420c0839e7f5e54dba5e6418
John Stratton <jstrat0407@gmail.com>
2017-10-25 01:24:55 -0400

    set MongoDB permissions and set to auto-start

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,8 @@ sudo apt install -y git
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y mongodb-org
+sudo mkdir /data/
+sudo mkdir /data/db
+sudo chmod 777 /data/db
+sudo systemctl enable mongod.service
 sudo service mongod start
