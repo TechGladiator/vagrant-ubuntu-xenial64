7c321d05f7a642793aa40622aee6cf87a4a885d0
John Stratton <jstrat0407@gmail.com>
2017-11-11 17:36:08 -0500

    use 'apt' instead of 'apt-get'
    because https://itsfoss.com/apt-vs-apt-get-difference/

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt-get update
+sudo apt update
 sudo apt install -y git
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt install -y nodejs
 sudo mkdir /home/ubuntu/.npm-global
 npm config set prefix '/home/ubuntu/.npm-global'
 echo 'prefix=~/.npm-global' >> /home/ubuntu/.npmrc
@@ -12,7 +12,7 @@ echo 'export PATH=~/.npm-global/bin:$PATH' >> /home/ubuntu/.profile
 npm i -g npm
 sudo chown -R ubuntu /home/ubuntu/.npm-global
 sudo chown -R ubuntu /home/ubuntu/.npmrc
-sudo apt-get install -y mongodb-org
+sudo apt install -y mongodb-org
 sudo mkdir /data/
 sudo mkdir /data/db
 sudo chmod 777 /data/db
